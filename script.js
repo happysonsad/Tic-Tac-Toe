fcf8ad3f82f578a1ffbc8b4dc4305d735949c005
happysonsad <176979803+happysonsad@users.noreply.github.com>
2024-10-09 16:03:50 +0800

    finish fisrt draft

--- script.js
+++ script.js
@@ -1,110 +1,89 @@
-// const Gameboard = () => {
-//     row = 3;
-//     column = 3;
-// }
-const Gameboard = (function() {
+const Gameboard = (() => {
     let board = ["", "", "", "", "", "", "", "", ""];
 
     const getBoard = () => board;
 
-    const updateBoard = function(index, marker) {
+    const updateBoard = (index, marker) => {
         if (board[index] === "") {
             board[index] = marker;
-            //DOM - textContent: update cell on the page
+            document.querySelector(`[data-index="${index}"]`).textContent = marker;
         }
     };
 
-    /*
-    const resetGame = function() {
+    const resetGame = () => {
         board = ["", "", "", "", "", "", "", "", ""];
         document.querySelectorAll(".cell").forEach(cell => {
             cell.textContent = "";
         });
     };
-    */
 
-    return { getBoard, updateBoard, /* resetGame */ }
+    return { getBoard, updateBoard, resetGame };
 
 })();
 
-// function createPlayer (name) {
-//     const playName = name;
 
-//     let score = 0;
-//     const getScore = () => score;
-//     const addScore = () => score++;
-
-//     return { name, getScore, addScore };
-// }
-const player = function(name, marker) {
+const player = (name, marker) => {
     const getName = () => name;
     const getMarker = () => marker;
     return { getName, getMarker };
 }
 
-// function gameFlow() {
-//     player 1 turn;
-//     player 2 turn;
-//     if someone win then gameover
-// }
 
-const gameFlow = (function () {
+const gameFlow = (() => {
 
     let currentPlayer;
     let playerOne;
     let playerTwo;
     let gameOver = false;
 
-    const startGame = function(nameOne, nameTwo) {
+    const startGame = (nameOne, nameTwo) => {
         playerOne = player(nameOne, "X");
         playerTwo = player(nameTwo, "O");
         currentPlayer = playerOne;
         gameOver = false;
         //DOM - textContent: current player's turn
+        document.querySelector(".playerTurnInfo").textContent = `${playerOne.getName()}'s turn`;
     };
 
-    const switchTurn = function() {
+    const switchTurn = () => {
         currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
         //DOM - textContent: current player's turn
+        document.querySelector(".playerTurnInfo").textContent = `${currentPlayer.getName()}'s turn`;
     };
 
     const getCurrentPlayer = () => currentPlayer;
 
-    const triggerGameOver = function(winner) {
+    const triggerGameOver = (winner) => {
         gameOver = true;
         //DOM - textContent: winner or draw
+        document.querySelector(".playerTurnInfo").textContent = winner ? `${winner.getName()} wins!!!` : `DRAW!!!`;
     }
 
     return { startGame, switchTurn, getCurrentPlayer, triggerGameOver };
 
 })();
 
-const runTurn = function(index) {
+const runTurn = (index) => {
     if (!gameFlow.isGameOver && Gameboard.getBoard()[index] === "") {
         const currentPlayer = gameFlow.getCurrentPlayer();
         Gameboard.updateBoard(index, currentPlayer.getMarker());
-    }
 
-    if(checkWinner()) {
-        gameFlow.triggerGameOver(currentPlayer);
-    } else if (checkDraw()) {
-        gameFlow.triggerGameOver(null);
-    } else {
-        gameFlow.switchTurn();
+        if (checkWinner()) {
+            gameFlow.triggerGameOver(currentPlayer);
+        } else if (checkDraw()) {
+            gameFlow.triggerGameOver(null);
+        } else {
+            gameFlow.switchTurn();
+        }
     }
-}
+};
 
-const checkWinner = function() {
+const checkWinner = () => {
     const board = Gameboard.getBoard();
     const winCondition = [
-        [0, 1, 2], 
-        [3, 4, 5], 
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6],
     ];
 
     for (let combo of winCondition) {
@@ -112,13 +91,31 @@ const checkWinner = function() {
         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
             return true;
         }
-        return false;
     }
-}
+    return false;
+};
 
 
-const checkDraw = function() {
+const checkDraw = () => {
     return Gameboard.getBoard().every(cell => cell !== "");
 }
 
+document.querySelector(".startGameButton").addEventListener("click", () => {
+    const nameOne = document.querySelector("#playerOne").value || "Player One";
+    const nameTwo = document.querySelector("#playerTwo").value || "Player Two";
+    gameFlow.startGame(nameOne, nameTwo)
+});
 
+document.querySelector(".resetGamebutton").addEventListener("click", () => {
+    Gameboard.resetGame();
+    const nameOne = document.querySelector("#playerOne").value;
+    const nameTwo = document.querySelector("#playerTwo").value;
+    gameFlow.startGame(nameOne, nameTwo);
+});
+
+document.querySelectorAll(".cell").forEach(cell => {
+    cell.addEventListener("click", (event) => {
+        const index = event.target.getAttribute("data-index");
+        runTurn(index);
+    });
+});
